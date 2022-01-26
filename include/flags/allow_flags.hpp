65027a5fd7f316fd35bd5e75478a9c072325e133
Tim Crews <tim.crews@code-affinity.com>
2022-01-26 16:41:58 -0500

    Apply patch described in https://github.com/grisumbras/enum-flags/issues/31

--- include/flags/allow_flags.hpp
+++ include/flags/allow_flags.hpp
@@ -7,18 +7,16 @@
 
 namespace flags {
 
-
-template <class E, class Enabler = void> struct is_flags
-: public std::false_type {};
-
+  template <typename T, typename = void>
+  struct is_flags : std:false_type {};
+  template <typename T>
+  struct is_flags < T, decltype(enableEnumFlags(T{})) > : std::true_type {};
 
 } // namespace flags
 
 
-#define ALLOW_FLAGS_FOR_ENUM(name) \
-namespace flags { \
-template <> struct is_flags< name > : std::true_type {}; \
-}
+#define ALLOW_FLAGS_FOR_ENUM(Name) inline void enableEnumFlags(Name) {}
+#define ALLOW_FLAGS_FOR_ENUM_IN_CLASS(Name) friend inline void enableEnumFlags(Name) {}
 
 
 #endif // ENUM_CLASS_ALLOW_FLAGS_HPP
